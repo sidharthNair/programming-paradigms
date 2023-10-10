e42e708bf8565f32e175c1e1ee81cafe65c4055d
Sidharth Nair <sidnair@utexas.edu>
2023-10-10 12:50:04 -0500

    Added go setup script and test file

---
+++ project/go.sh
@@ -0,0 +1,16 @@
+echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
+
+if ! which go &> /dev/null
+then
+    if ! [ -d /usr/local/go ]; then
+        echo "Installing go ..."
+        wget https://dl.google.com/go/go1.21.3.linux-amd64.tar.gz
+        sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.21.3.linux-amd64.tar.gz
+        export PATH=$PATH:/usr/local/go/bin
+        echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
+        rm go1.21.3.linux-amd64.tar.gz
+    fi
+    export PATH=$PATH:/usr/local/go/bin
+fi
+
+go version
